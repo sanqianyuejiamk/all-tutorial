2a0fae0089f5ae0890ff424a9460e74c9f42f88e
hyy044101331 <hyy044101331@163.com>
2017-12-01 17:44:16 +0800

    update

--- springboot-mvc-consul/src/main/java/com/mengka/springboot/config/MyPropertySourceLocator.java
+++ springboot-mvc-consul/src/main/java/com/mengka/springboot/config/MyPropertySourceLocator.java
@@ -2,6 +2,7 @@ package com.mengka.springboot.config;
 
 import com.google.common.collect.Maps;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.PropertySource;
 
@@ -13,7 +14,20 @@ public class MyPropertySourceLocator implements PropertySourceLocator {
 
     @Override
     public PropertySource<?> locate(Environment environment) {
-        ConsulPropertySource myPropertySource = new ConsulPropertySource("myPropertySource", Maps.newHashMap());
-        return myPropertySource;
+        if (environment instanceof ConfigurableEnvironment) {
+            ConfigurableEnvironment env = (ConfigurableEnvironment) environment;
+            String serviceName = env.getProperty("mengka.cloud.service.name");
+            String serviceTag = env.getProperty("service.tag");
+            if ("local".equalsIgnoreCase(serviceTag)) {
+                return null;
+            } else {
+                String consulKey = String.format("service/%s/%s/config", new Object[]{serviceName, serviceTag});
+
+                ConsulPropertySource myPropertySource = new ConsulPropertySource("myPropertySource", Maps.newHashMap());
+                return myPropertySource;
+            }
+        } else {
+            return null;
+        }
     }
 }
