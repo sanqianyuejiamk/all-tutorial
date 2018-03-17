6d4a2f738e38c9fa6761c0b19766b0b4da4667a8
hyy044101331 <934734735@qq.com>
2018-03-18 00:36:16 +0800

    spring boot restTemplate请求超时时间设置

--- springboot-mvc-velocity-quasar/src/main/java/com/mengka/springboot/config/AppConfig.java
+++ springboot-mvc-velocity-quasar/src/main/java/com/mengka/springboot/config/AppConfig.java
@@ -1,9 +1,24 @@
-package com.mengka.springboot.config;
-
-/**
- * @author huangyy
- * @version cabbage-forward2.0,2018-03-18
- * @since cabbage-forward2.0
- */
-public class AppConfig {
-}
+//package com.mengka.springboot.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+//import org.springframework.web.client.RestTemplate;
+//
+///**
+// * @author huangyy
+// * @version cabbage-forward2.0,2018-03-18
+// * @since cabbage-forward2.0
+// */
+//@Configuration
+//public class AppConfig {
+//
+//    @Bean
+//    public RestTemplate customRestTemplate(){
+//        HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+//        httpRequestFactory.setConnectionRequestTimeout(-1);
+//        httpRequestFactory.setConnectTimeout(300000);
+//        httpRequestFactory.setReadTimeout(300000);
+//        return new RestTemplate(httpRequestFactory);
+//    }
+//}
